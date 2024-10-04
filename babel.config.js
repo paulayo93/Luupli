202a3f19faa19e077c223995672b07dcd7bcf9dd
paulayo93 <52790286+paulayo93@users.noreply.github.com>
2024-10-04 21:18:41 +0100

    setup project and add features

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,38 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    ['@babel/plugin-transform-private-methods', {
+      'loose': true,
+    }],
+    [
+
+      'module-resolver',
+
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+
+          '@images': './assets/images',
+          '@icons': './assets/icons/*',
+          '@components': './src/components',
+          '@store': './src/store',
+          '@utils': './src/utils',
+          '@screens/*': './src/screens/*',
+          '@types': './src/types',
+        },
+      },
+    ],
     'react-native-reanimated/plugin',
   ],
 };
